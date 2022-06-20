2f970f0725a4ea3f18b4783cda57fd8ccf54e952
Martin Šafránek <martin.safranek22@seznam.cz>
2022-06-20 14:45:30 +0200

    jquery komplet

--- script.js
+++ script.js
@@ -5,5 +5,21 @@
             $("html, body").animate({scrollTop: $(".n1").offset().top}, 500);
         });
 
+        $(".n2-scroll").click(function(){
+            $("html, body").animate({scrollTop: $(".n2").offset().top}, 600);
+        });
+
+        $(".content-scroll").click(function(){
+            $("html, body").animate({scrollTop: $(".tip").offset().top}, 700);
+        });
+
+        $(".n3-scroll").click(function(){
+            $("html, body").animate({scrollTop: $(".n3").offset().top}, 700);
+        });
+
+        $(".n4-scroll").click(function(){
+            $("html, body").animate({scrollTop: $(".n4").offset().top}, 800);
+        });
+
     });
 })(jQuery);
